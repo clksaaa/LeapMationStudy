193a44fbbecbfd854fe33c288b1110546b98146a
chenlukun <1043656368@qq.com>
2021-02-01 10:32:01 +0800

    导入默认工程 提交到库

---
+++ Assets/LeapMationTools/Scripts/Editor/AddObjectToJointEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AddObjectToJoint))]
+public class AddObjectToJointEditor : Editor
+{
+  public override void OnInspectorGUI()
+  {
+    base.OnInspectorGUI();
+    if(GUILayout.Button("挂在物体到所有的关节下"))
+    {
+      //Debug.Log("点击了");
+      (target as AddObjectToJoint).Add();
+    }
+    if(GUILayout.Button("删除挂在物体关节下生成的物体"))
+    {
+      //Debug.Log("点击了");
+      (target as AddObjectToJoint).Clear();
+    }
+  }
+}
---
+++ Assets/LeapMationTools/Scripts/Util/AddObjectToJoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Leap.Unity.Attachments;
+using UnityEngine;
+
+[RequireComponent(typeof(AttachmentHands))]
+public class AddObjectToJoint : MonoBehaviour
+{
+    public GameObject addObg;
+    public List<GameObject> addList = new List<GameObject>();
+    public void Add()
+    {
+        if (addObg != null)
+        {
+            foreach (var obj in addList)
+            {
+                DestroyImmediate(obj.gameObject);
+            }
+            addList.Clear();
+        }
+        foreach (Transform item in transform)
+        {
+            foreach (Transform child in item.GetComponentsInChildren<Transform>())
+            {
+                if(child==item) continue;
+                
+                //Debug.Log(child.name,child.gameObject);
+                var temp= Instantiate(addObg, child);
+                addList.Add(temp);
+            }
+        }
+    }
+
+    public void Clear()
+    {
+        if (addObg != null)
+        {
+            foreach (var obj in addList)
+            {
+                DestroyImmediate(obj.gameObject);
+            }
+            addList.Clear();
+        }
+    }
+    
+}
